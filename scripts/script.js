bc331d33f9c8a2e1bab2b334e3e628126f1b75aa
Vugar Seidov <vugar.sagidov2017@yandex.ru>
2023-02-01 01:06:57 +0500

    вёрстка сайта

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile-info__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let form = document.querySelector('.popup__container');
+let infoTitle = document.querySelector('.profile-info__title');
+let infoSubtitle = document.querySelector('.profile-info__subtitle');
+let inputName = document.querySelector('.popup__input_name');
+let inputDescription = document.querySelector('.popup__input_description');
+let saveButton = document.querySelector('.popup__save-button');
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+editButton.addEventListener('click', openPopup);
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+closeButton.addEventListener('click', closePopup);
+function handleFormSumbit(evt) {
+  evt.preventDefault();
+  infoTitle.textContent = inputName.value;
+  infoSubtitle.textContent = inputDescription.value;
+  closePopup()
+}
+form.addEventListener("submit", handleFormSumbit);
